d739a4ec702ce18fad748e34d0764da09ac61588
MateusLima <36885785+MateusLima011@users.noreply.github.com>
2022-02-22 17:40:56 -0300

    Singleton Test

--- app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -1,9 +1,23 @@
 package com.example.convidados.service.repository
 
+import android.content.Context
 import com.example.convidados.service.model.GuestModel
 import java.util.*
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context){
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object{
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context) : GuestRepository{
+            if (::repository.isInitialized){
+                repository = GuestRepository(context)
+            }
+            return GuestRepository(context)
+        }
+    }
 
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
@@ -21,6 +35,7 @@ class GuestRepository {
     }
 
     fun save(guest: GuestModel) {
+       mGuestDataBaseHelper.writableDatabase
     }
 
     fun delete(guest: GuestModel) {
--- app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
@@ -1,16 +1,18 @@
 package com.example.convidados.viewmodel
 
+import android.app.Application
 import android.widget.EditText
 import android.widget.RadioButton
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.convidados.service.model.GuestModel
 import com.example.convidados.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    private var mGuestRepository: GuestRepository = GuestRepository()
+    private val mContext = application.applicationContext
+    private var mGuestRepository: GuestRepository = GuestRepository.getInstance(mContext)
 
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest
